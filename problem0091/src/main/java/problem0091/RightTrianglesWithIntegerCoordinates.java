36269f13fa6c395b1d43ab4770e0ee138b4e36dd
Ilya Kurnosov <kurniliya@gmail.com>
2019-10-17 01:38:30 +0300

    Refactored for simplicity

--- problem0091/src/main/java/problem0091/RightTrianglesWithIntegerCoordinates.java
+++ problem0091/src/main/java/problem0091/RightTrianglesWithIntegerCoordinates.java
@@ -30,44 +30,30 @@ class RightTrianglesWithIntegerCoordinates {
     // P (x1, y1) iterates over whole space.
     for (int x1 = 0; x1 <= maxCoordinate; x1++) {
       for (int y1 = 0; y1 <= maxCoordinate; y1++) {
-        final Vector p = new Vector(x1, y1);
-        result += considerComplimentaryVectors(p, maxCoordinate);
-      }
-    }
+        // Q (x2, y2) iterates over whole space.
+        for (int x2 = 0; x2 <= maxCoordinate; x2++) {
+          for (int y2 = 0; y2 <= maxCoordinate; y2++) {
+            final Vector p = new Vector(x1, y1);
+            final Vector q = new Vector(x2, y2);
 
-    return result;
-  }
-
-  private static int considerComplimentaryVectors(final Vector p,
-      final int maxCoordinate) {
-    // Carefully iterate over complimentary Q (x2, y2) in such a way that
-    // we don't consider equivalent triangles twice in the end of the day.
-    //
-    // I.e. 3!=6 possible permutations of {(0, 0), (0, 1), (1, 0)} are exactly
-    // the same thing, hence, must be counted only once.
-    // Origin (0, 0) figures in the implemented algorithm only implicitly,
-    // so we have to only protect against miscounting remaining 2!=2
-    // permutations of {(0, 1), (1, 0)}.
-    int result = 0;
-
-    // At the same vertical level, consider only points to the right.
-    int y2 = p.y;
-    for (int x2 = p.x + 1; x2 <= maxCoordinate; x2++) {
-      final Vector q = new Vector(x2, y2);
-      if (formRightTriangle(p, q)) {
-        result++;
-      }
-    }
-
-    // At higher vertical levels, consider all the points.
-    for (y2 = p.y + 1; y2 <= maxCoordinate; y2++) {
-      for (int x2 = 0; x2 <= maxCoordinate; x2++) {
-        final Vector q = new Vector(x2, y2);
-        if (formRightTriangle(p, q)) {
-          result++;
+            if (formRightTriangle(p, q)) {
+              result += 1;
+            }
+          }
         }
       }
     }
+
+    // Account for the fact that the implementation considers equivalent
+    // permutations -- e.g. {(0, 1), (1, 0)} and {(1, 0), (0, 1)} -- twice,
+    // as both P and Q iterate over whole space.
+    //
+    // Alternative would be iterating more carefully to avoid generating
+    // equivalent cases twice in the first place. Such approach would make
+    // for more complex iteration logic. Performance of the chosen version is
+    // good enough for final problem set, so complication isn't warranted.
+    assert result % 2 == 0 : result;
+    result /= 2;
 
     return result;
   }
