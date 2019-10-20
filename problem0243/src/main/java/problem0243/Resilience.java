4b3d02ed9b4bb5685b8d0e8a1effa5d705868707
Ilya Kurnosov <kurniliya@gmail.com>
2019-10-21 02:34:58 +0300

    Problem 243 "Resilience"
    Smarter version: too slow still

--- problem0243/src/main/java/problem0243/Resilience.java
+++ problem0243/src/main/java/problem0243/Resilience.java
@@ -2,7 +2,10 @@ package problem0243;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-import java.util.List;
+import com.google.common.collect.Sets;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import utils.Factorization;
 import utils.PrimeFactor;
 
@@ -11,8 +14,8 @@ import utils.PrimeFactor;
  * <h2>Problem 243</h2>
  * A positive fraction whose numerator is less than its denominator is called a
  * proper fraction. For any denominator, d, there will be d−1 proper fractions;
- * for example, with d = 12: 1/12, 2/12, 3/12, 4/12, 5/12, 6/12, 7/12,
- * 8/12, 9/12, 10/12, 11/12.
+ * for example, with d = 12: 1/12, 2/12, 3/12, 4/12, 5/12, 6/12, 7/12, 8/12,
+ * 9/12, 10/12, 11/12.
  * <p>
  * We shall call a fraction that cannot be cancelled down a resilient fraction.
  * Furthermore we shall define the resilience of a denominator, R(d), to be the
@@ -28,6 +31,7 @@ public class Resilience {
       final long ratioDenominator) {
     checkArgument(ratioNumerator > 0);
     checkArgument(ratioDenominator > 1);
+    long time = System.nanoTime();
 
     for (long candidate = 2; candidate < Long.MAX_VALUE; ++candidate) {
       final long resilientFractionsCount = countResilientFractions(candidate);
@@ -37,37 +41,63 @@ public class Resilience {
           < properFractionsCount * ratioNumerator) {
         return candidate;
       }
+      final long step = 1_000_000;
+      if (candidate % step == 0) {
+        final long elapsed = System.nanoTime() - time;
+        System.out.println(
+            String
+                .format("%d sec: %d * %d",
+                    TimeUnit.NANOSECONDS.toSeconds(elapsed),
+                    candidate / step, step));
+        time = System.nanoTime();
+      }
     }
 
     return -1;
   }
 
+  /**
+   * Returns number of resilient fractions for given denominator.
+   * <p>
+   * Implementations idea:
+   * <p>
+   * Find prime factorization of the denominator. Then all the numerators
+   * divisible by one of the prime factors are non-resilient, and the rest are
+   * resilient.
+   * <p>
+   * First, we compute the number of numerators divisible by each of prime
+   * factors by dividing number of all the possible numerators (n-1) by the
+   * value of the factor. This procedure counts more than once every numerator
+   * that is divisible by more than one of denominators's prime factors.
+   * <p>
+   * We then correct for this over-counting directly, by additionally computing
+   * number of numerators that have been counted more than once, i.e. those
+   * divisible by some combination of several prime factors.
+   *
+   * @param n Denominator to count number of resilient fractions for.
+   * @return Number of resilient factions.
+   */
   static long countResilientFractions(final long n) {
     checkArgument(n >= 2);
 
-    long result = 1;  // 1/n is always a resilient fraction.
-    final List<PrimeFactor> factors = Factorization.of(n).factors();
-    final long[] primeFactors = factors.stream()
+    final Set<Long> factors = Factorization.of(n).factors().stream()
         .mapToLong(PrimeFactor::factor)
-        .toArray();
+        .boxed()
+        .collect(Collectors.toSet());
+    long nonResilientCount = 0;
 
-    for (long i = 2; i < n; ++i) {
-      if (isResilientFraction(i, primeFactors)) {
-        result++;
+    Set<Set<Long>> factorCombinations = Sets.powerSet(factors);
+    for (var factorCombination : factorCombinations) {
+      if (factorCombination.size() == 0) {
+        // Ignore empty set.
+        continue;
       }
+      long divisor = factorCombination.stream()
+          .reduce(1L, (left, right) -> left * right);
+      final long sign = factorCombination.size() == 1 ? +1 : -1;
+      nonResilientCount += sign * (n - 1) / divisor;
     }
 
-    return result;
-  }
-
-  private static boolean isResilientFraction(final long numerator,
-      final long[] denominatorPrimeFactors) {
-    for (long factor : denominatorPrimeFactors) {
-      if (numerator % factor == 0) {
-        return false;
-      }
-    }
-
-    return true;
+    return (n - 1) - nonResilientCount;
   }
 }
--- problem0243/src/test/java/problem0243/ResilienceTest.java
+++ problem0243/src/test/java/problem0243/ResilienceTest.java
@@ -13,6 +13,21 @@ public class ResilienceTest {
   }
 
   @Test
+  public void countResilientFractions_11() {
+    assertThat(Resilience.countResilientFractions(11), is(10L));
+  }
+
+  @Test
+  public void countResilientFractions_30() {
+    assertThat(Resilience.countResilientFractions(30), is(8L));
+  }
+
+  @Test
+  public void countResilientFractions_997() {
+    assertThat(Resilience.countResilientFractions(997), is(996L));
+  }
+
+  @Test
   public void smallestDenominatorWithResilienceLessThan_4_10() {
     assertThat(Resilience.smallestDenominatorWithResilienceLessThan(4, 10),
         is(12L));
