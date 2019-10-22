0637bd92eb7feb941647a59f8cfc128847fb2da6
Ilya Kurnosov <kurniliya@gmail.com>
2019-10-22 03:15:41 +0300

    Problem 15 "Lattice paths"
    Recursive solution: ridiculously slow

---
+++ problem0015/src/main/java/problem0015/App.java
@@ -0,0 +1,18 @@
+package problem0015;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println(f(20, 20));
+  }
+
+  static long f(final long m, final long n) {
+    if (n == 0 && m == 0) {
+      return 0;
+    } else if (m == 0 || n == 0) {
+      return 1;
+    }
+
+    return f(m, n - 1) + f(m - 1, n);
+  }
+}
---
+++ problem0015/src/test/java/problem0015/AppTest.java
@@ -0,0 +1,26 @@
+package problem0015;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void test_0x0() {
+        assertEquals(0, App.f(0, 0));
+    }
+
+    @Test public void test_1x1() {
+        assertEquals(2, App.f(1, 1));
+    }
+
+    @Test public void test_1x2() {
+        assertEquals(3, App.f(1, 2));
+    }
+
+    @Test public void test_2x1() {
+        assertEquals(3, App.f(2, 1));
+    }
+
+    @Test public void test_2x2() {
+        assertEquals(6, App.f(2, 2));
+    }
+}
